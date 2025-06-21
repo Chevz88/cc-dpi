c973d059d8a0d46e4c86c8eb2cfb3d2a669b6aff
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-21 10:53:44 +0700

    Update NavGraph.kt

--- CC-DPI-App/app/src/main/java/com/pekerja/app/navigation/NavGraph.kt
+++ CC-DPI-App/app/src/main/java/com/pekerja/app/navigation/NavGraph.kt
@@ -1,1 +1,16 @@
-// Navigation setup
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = "splash") {
+        composable("splash") { SplashScreen(navController) }
+        composable("login") { LoginScreen(navController) }
+        composable("register") { RegisterScreen(navController) }
+        composable("dashboard") { DashboardScreen(navController) }
+        composable("aduan") { AduanFormScreen(navController) }
+        composable("riwayat") { RiwayatScreen(navController) }
+        composable("edukasi") { EdukasiScreen(navController) }
+        composable("chatbot") { ChatbotScreen(navController) }
+        composable("profil") { ProfilScreen(navController) }
+        composable("info") { InfoPekerjaScreen(navController) }
+        composable("tentang") { TentangScreen(navController) }
+    }
+}
