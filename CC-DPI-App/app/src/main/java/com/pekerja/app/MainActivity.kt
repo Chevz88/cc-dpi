61ca0b4fc301f8e729dd0d66ef73bc52b932612e
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-21 10:55:22 +0700

    Update MainActivity.kt

--- CC-DPI-App/app/src/main/java/com/pekerja/app/MainActivity.kt
+++ CC-DPI-App/app/src/main/java/com/pekerja/app/MainActivity.kt
@@ -1,1 +1,11 @@
-// MainActivity placeholder
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
+            }
+        }
+    }
+}
