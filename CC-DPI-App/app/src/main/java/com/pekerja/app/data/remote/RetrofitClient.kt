db407b336f9b189cf6d2b86296a5e2519f240a4c
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-21 12:07:00 +0700

    Update RetrofitClient.kt

--- CC-DPI-App/app/src/main/java/com/pekerja/app/data/remote/RetrofitClient.kt
+++ CC-DPI-App/app/src/main/java/com/pekerja/app/data/remote/RetrofitClient.kt
@@ -1,1 +1,19 @@
-// Retrofit client
+package com.pekerja.app.data.remote
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://192.168.0.7:5000/" // Ganti dengan ngrok URL jika dibutuhkan
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val chatbotApi: ChatbotApi by lazy {
+        retrofit.create(ChatbotApi::class.java)
+    }
+}
